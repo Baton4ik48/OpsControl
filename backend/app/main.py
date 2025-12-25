113f0d6ddc5cf3516c3cdaf74ab702b4b9e78269
Lenovo(Natasha) <otrokov48@gmail.com>
2025-12-25 21:53:43 +0300

    day 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
-from app.middleware.allowed_user import AllowedNetworkMiddleware
+from fastapi.staticfiles import StaticFiles
+from app.api import health
+from app.middleware.allowed_network import AllowedNetworkMiddleware
 from app.config import settings
 
-app = FastAPI()
-
 allowed_networks = settings.YAML["security"]["allowed_networks"]
+
+
+app = FastAPI()
 
 app.add_middleware(
     AllowedNetworkMiddleware,
     allowed_networks=allowed_networks
 )
 
-@app.get("/health")
-def health():
-    return {
-        "status": "ok",
-        "env": settings.YAML["app"]["name"]
-    }
+app.include_router(health.router)
