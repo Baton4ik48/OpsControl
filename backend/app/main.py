8fab9a9bb056eeee00b2ba86b125ccbfcfb8bb92
UserWork <user@LAPTOP-ANDREY>
2026-02-05 22:34:50 +0300

    Добавлена проверка статуса сервера и сервисов

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api.api_database import router as db_router
-from app.api.api_health import router as health_router
+from app.api.api_status import router as status_router
 from app.api.api_credentials import router as credentials_router
 from app.middleware.allowed_network import AllowedNetworkMiddleware
 from app.config import settings
@@ -14,6 +14,6 @@ app.add_middleware(
     allowed_networks=allowed_networks
 )
 
-app.include_router(health_router)
+app.include_router(status_router)
 app.include_router(db_router)
 app.include_router(credentials_router)
