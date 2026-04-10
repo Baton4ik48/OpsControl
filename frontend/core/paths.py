7b2728b7281d782e2f20565fa873f3ab83c4b0ec
Andrei <otrokov48@gmail.com>
2026-04-10 13:25:43 +0300

    Мелкие исправления

--- frontend/core/paths.py
+++ frontend/core/paths.py
@@ -1,19 +1,24 @@
 import os
+import sys
 
-FRONTEND_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..")
-)
 
-RESOURCES_DIR = os.path.join(FRONTEND_DIR, "resources")
-CORE_DIR = os.path.join(FRONTEND_DIR, "core")
+def get_base_path():
+    if hasattr(sys, '_MEIPASS'):
+        return sys._MEIPASS
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+BASE_DIR = get_base_path()
+
+RESOURCES_DIR = os.path.join(BASE_DIR, "resources")
+CORE_DIR = os.path.join(BASE_DIR, "core")
 CONFIG_DIR = os.path.join(CORE_DIR, "config")
 ICONS_DIR = os.path.join(RESOURCES_DIR, "icons")
 TEMPLATES_DIR = os.path.join(RESOURCES_DIR, "templates")
-
-
 
 FIREWALL_TEMPLATE_PATH = os.path.join(
     TEMPLATES_DIR,
     "firewall_template.xlsx"
 )
-LOG_DIR = os.path.join(FRONTEND_DIR, "logs")
+
+LOG_DIR = os.path.join(BASE_DIR, "logs")
