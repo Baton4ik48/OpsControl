5b36fac55e95f9fea8e5fd48f9e19be1e238805c
Andrei <otrokov48@gmail.com>
2026-04-14 17:18:23 +0300

    изменения в компос для ваулт  астра линукс

--- backend/migrate_add_device_type.sql
+++
@@ -1,6 +0,0 @@
--- Миграция: добавление типа устройства к серверам
--- Запустить один раз на существующей базе данных:
---   psql -U <user> -d <db> -f migrate_add_device_type.sql
-
-ALTER TABLE servers
-    ADD COLUMN IF NOT EXISTS device_type VARCHAR(32) NOT NULL DEFAULT 'linux';
